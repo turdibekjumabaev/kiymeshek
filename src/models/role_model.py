a6b1e7d766b57fa408c4ccbf7c80cedac159c78b
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-09-25 17:06:46 +0500

    Role model

---
+++ src/models/role_model.py
@@ -0,0 +1,19 @@
+from src.models import db
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String)
+
+    def __init__(self, role_name):
+        self.role_name = role_name
+
+    def __repr__(self):
+        return f'<Role {self.role_name}>'
+
+    def to_dict(self):
+        return {
+            "role_id": self.id,
+            "role_name": self.role_name
+        }
